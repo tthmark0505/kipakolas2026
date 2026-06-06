066b1e034210e18fa4b95a1002d6630d9a9bd3f4
Mark Benjamin Toth <tothmarkyb@gmail.com>
2026-06-06 13:16:05 +0200

    Véglegesítés

--- components/Masonry/Masonry.tsx
+++ components/Masonry/Masonry.tsx
@@ -9,21 +9,64 @@ const blocks = [
   "row-span-1",
 ];
 
+const images = [
+  "/masonry-1.png",
+  "/masonry-2.png",
+  "/masonry-3.png",
+  "/masonry-4.png",
+  "/masonry-5.png",
+  "/masonry-6.png",
+  "/masonry-7.png",
+  "/masonry-8.png",
+];
+
 export default function Masonry() {
   return (
     <div className="grid h-[680px] grid-cols-3 grid-rows-6 gap-3">
       {blocks.map((size, index) => (
         <div
           key={index}
-          className={`${size} group relative overflow-hidden rounded-md`}
+          className={`${size} group relative overflow-hidden rounded-xl`}
         >
           <img
-            src={`https://picsum.photos/600/800?random=${index + 1}`}
-            alt=""
-            className="h-full w-full object-cover transition-all duration-500 ease-out group-hover:scale-105"
+            src={images[index]}
+            alt={`Masonry ${index + 1}`}
+            className="
+              h-full
+              w-full
+              object-cover
+              transition-all
+              duration-500
+              ease-out
+              group-hover:scale-105
+            "
           />
 
-          <div className="absolute inset-0 bg-black/0 transition-all duration-300 group-hover:bg-black/20" />
+          {/* Kékes színezés */}
+          <div
+            className="
+              absolute
+              inset-0
+              bg-[#5d9dff]
+              mix-blend-soft-light
+              opacity-40
+              transition-all
+              duration-300
+              group-hover:opacity-20
+            "
+          />
+
+          {/* Sötétítés */}
+          <div
+            className="
+              absolute
+              inset-0
+              bg-black/25
+              transition-all
+              duration-300
+              group-hover:bg-black/10
+            "
+          />
         </div>
       ))}
     </div>
